3ecded0fdd438f4b6d6baa0335e556651b07f2f7
David Robbins <robbins.david@gmail.com>
2011-11-15 14:22:05 -0600

    Added crumbs submodule and assorted scripts

---
+++ _attachments/loader.js
@@ -0,0 +1,20 @@
+
+function app_load(scripts) {
+  for (var i=0; i < scripts.length; i++) {
+    document.write('<script src="'+scripts[i]+'"><\/script>')
+  };
+};
+
+app_load([
+  "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js",
+  //"http://documentcloud.github.com/underscore/underscore-min.js",
+  "scripts/d3.min.js",
+  "scripts/d3.behavior.min.js",
+  "scripts/d3.chart.min.js",
+  "scripts/d3.csv.min.js",
+  "scripts/d3.geo.min.js",
+  "scripts/d3.geom.min.js",
+  "scripts/d3.layout.min.js",
+  "scripts/d3.time.min.js",
+  "scripts/jquery.mustache.js" //my fixed version of mustache.js. Check for updates later.
+]);
