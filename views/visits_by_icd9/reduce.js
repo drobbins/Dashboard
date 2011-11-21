9f8be75ecb3ece09ea6f4730ea266db3af878aa7
David Robbins <robbins.david@gmail.com>
2011-11-21 10:47:58 -0600

    Added ICD-9 views

---
+++ views/visits_by_icd9/reduce.js
@@ -0,0 +1,7 @@
+function(keys, values, rereduce){
+    var first_value = values[0],
+        labels = first_value.labels;
+        count = sum(values.map(function(v){return v.values[0];})),
+        value = [count];
+    return { "values" : [count], "labels" : labels};
+}
