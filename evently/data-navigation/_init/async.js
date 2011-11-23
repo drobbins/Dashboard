cb95ca899e6b7a4d0f9df7fb1ce5b5c45fd5d9c2
David Robbins <robbins.david@gmail.com>
2011-11-23 07:22:33 -0600

    Hide the patient_names view. Load couchdb login locally

--- evently/data-navigation/_init/async.js
+++ evently/data-navigation/_init/async.js
@@ -2,6 +2,7 @@ function(callback, e, options){
     var views = $$(this).app.ddoc.views;
     if(views.lib){
         delete views.lib;
+        delete views.patient_names;
     }
     callback(views);
 }
