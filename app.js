fd4744f9f138711eab6844a18be08e1aeb351e98
David Robbins <robbins.david@gmail.com>
2011-12-01 20:00:38 -0600

    Load attachments from any _attachments sub-directories.

--- app.js
+++ app.js
@@ -13,6 +13,22 @@ requirejs(['couchapp', 'path', 'fs'], function(couchapp, path, fs){
     };
 
     module.exports = ddoc;
+    function loadAllAttachments(ddoc, dir){
+        var listings = fs.readdirSync(dir);
+        listings.forEach(function(listing){
+            var file = path.join(dir, listing),
+                stat = fs.statSync(file),
+                prefix = "";
+            if(stat.isFile()){return;}
+            if(path.basename(file) === "_attachments"){
+                prefix = path.dirname(file).slice(__dirname.length+1);
+                couchapp.loadAttachments(ddoc, file, prefix);
+            } else {
+                loadAllAttachments(ddoc, file);
+            }
+        });
+    }
 
     couchapp.loadAttachments(ddoc, path.join(__dirname, '_attachments'));
+    loadAllAttachments(ddoc, __dirname);
 });
