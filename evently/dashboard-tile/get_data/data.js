12986bebd0de0d3d08b80a176879cddb1fc62f29
David Robbins <robbins.david@gmail.com>
2012-01-12 16:18:57 -0600

    Prettied up the titles for the tiles.

--- evently/dashboard-tile/get_data/data.js
+++ evently/dashboard-tile/get_data/data.js
@@ -15,6 +15,10 @@ function(data){
     data_out['value_labels'] = first_row.value.labels.value_labels;
     data_out['rows'] = data.rows;
     data_out['view'] = $(this).data('view');
+    //Make the view name human readable for the title.
+    data_out['view_pretty'] = data_out.view.split("_").map(function(s){
+      return s.replace(/^./,s.charAt(0).toUpperCase());
+    }).join(" ");
     data_out['url'] = $(this).data('url');
     $(this).data('dashboard_data', data_out);
     return data_out;
