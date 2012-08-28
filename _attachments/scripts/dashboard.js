1e6117564e9d018b57b2dd2ba0eb101543395b0f
David Robbins <robbins.david@gmail.com>
2012-08-28 12:53:46 -0500

    Moved form post-processing to dashboard.js

--- _attachments/scripts/dashboard.js
+++ _attachments/scripts/dashboard.js
@@ -1,16 +1,28 @@
+'use strict';
+/*jshint browser:true jquery:true globalstrict:true*/
+/*globals $$:false */
+
 var toggleSpinner = function() {
-                var spinner = document.getElementById('spinner');
-                if (!spinner) {
-                    spinner = document.createElement('img');
-                    spinner.setAttribute('src', location.origin + '/_utils/image/spinner.gif');
-                    spinner.setAttribute('id', 'spinner');
-                    spinner.setAttribute('style', 'display: show');
-                    document.body.getElementsByTagName('div')[2].appendChild(spinner);
-                } else {
-                    if (spinner.getAttribute('style').indexOf('none') == -1) {
-                        spinner.setAttribute('style', 'display: none');
-                    } else {
-                        spinner.setAttribute('style', 'display: show');
+    var spinner = document.getElementById('spinner');
+    if (!spinner) {
+        spinner = document.createElement('img');
+        spinner.setAttribute('src', location.origin + '/_utils/image/spinner.gif');
+        spinner.setAttribute('id', 'spinner');
+        spinner.setAttribute('style', 'display: show');
+        document.body.getElementsByTagName('div')[2].appendChild(spinner);
+    } else {
+        if (spinner.getAttribute('style').indexOf('none') == -1) {
+            spinner.setAttribute('style', 'display: none');
+        } else {
+            spinner.setAttribute('style', 'display: show');
+        }
+    }
+};
+
+var IMCCP = {};
+
+IMCCP.populateFields = function () {
                     }
                 }
             };
+};
