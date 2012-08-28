d4744e0f249afbec42a918600e0f424021f4eed2
David Robbins <robbins.david@gmail.com>
2012-08-28 12:55:28 -0500

    FIxed bug in Autofill code.

--- _attachments/scripts/dashboard.js
+++ _attachments/scripts/dashboard.js
@@ -22,7 +22,27 @@ var toggleSpinner = function() {
 var IMCCP = {};
 
 IMCCP.populateFields = function () {
+    // Enable Autofill Fields
+    $(".autofill").autocomplete({
+        source : function(request, response){
+            var db = $$($("#main")).app.db,
+                design_doc_name = $$($("#main")).app.ddoc._id.split("/")[1],
+                field_name = $(this.element).attr("name"),
+                term = $(this.element).val(),
+                nonce = Math.random(),
+                that = this;
+            $$(that).nonce = nonce;
+            db.view(design_doc_name+"/autofill_"+field_name, {
+                startkey : term,
+                endkey : term+"\u9999", //I don't know why only \u9999 works, not \uFFFF
+                limit : 10,
+                group : true,
+                success : function(results){
+                    if($$(that).nonce === nonce){
+                        response(results.rows.map(function(row){return row.key;}));
                     }
                 }
-            };
+            });
+        }
+    });
 };
