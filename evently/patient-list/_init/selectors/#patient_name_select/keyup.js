5e35c27cdcfdfe5d0bae0e722dced792d928277c
David Robbins <robbins.david@gmail.com>
2012-08-28 08:12:56 -0500

    Fix a bug introduced by jquery.couch.js included with CouchDB 1.2, which changes the arguments db.list() expects.

--- evently/patient-list/_init/selectors/#patient_name_select/keyup.js
+++ evently/patient-list/_init/selectors/#patient_name_select/keyup.js
@@ -5,15 +5,20 @@ function(){
       design_doc_name = $$($("#main")).app.ddoc._id.split("/")[1],
   $$($(this)).nonce = nonce;
   db.list(design_doc_name+"/patient_names","patient_names", {
-    startkey : term,
-    endkey : term+"\u9999", //I don't know why only \u9999 works, not \uFFFF
-    limit : 25,
-    success : function(names){
-      if($$($(this)).nonce = nonce){
-      $("#select_results").html(names.rows.map(function(r){
-        //debugger;
-        return '<tr><td><a href="#'+r.id+'" class="patient">'+r.value.name+'</a></td></tr>';
-      }).join(""));
-      $("#select_results a").click(function(){$(this).trigger('edit_patient', $(this).attr('href'));});
-  }}});
+      startkey : term,
+      endkey : term+"\u9999", //I don't know why only \u9999 works, not \uFFFF
+      limit : 25
+    },
+    {
+      success : function(names){
+        if($$($(this)).nonce = nonce){
+          $("#select_results").html(names.rows.map(function(r){
+            //debugger;
+            return '<tr><td><a href="#'+r.id+'" class="patient">'+r.value.name+'</a></td></tr>';
+          }).join(""));
+          $("#select_results a").click(function(){$(this).trigger('edit_patient', $(this).attr('href'));});
+        }
+      }
+    }
+  );
 }
