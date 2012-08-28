fd60279acde778b687547be6cafa01bc26398de4
David Robbins <robbins.david@gmail.com>
2012-08-28 12:58:39 -0500

    Request 2: Enter/Return key should toggle between fields.

--- _attachments/scripts/dashboard.js
+++ _attachments/scripts/dashboard.js
@@ -22,6 +22,25 @@ var toggleSpinner = function() {
 var IMCCP = {};
 
 IMCCP.populateFields = function () {
+
+    //Use Enters as Tabs
+    $('input, select, textarea').bind('keydown', function(e) {
+        var self = $(this)
+          , form = self.parents('form:eq(0)')
+          , focusable
+          , next
+          ;
+        if (e.keyCode == 13) {
+            focusable = form.find('input,a,select,button,textarea').filter(':visible');
+            next = focusable.eq(focusable.index(this)+1);
+            if (next.length) {
+                next.focus();
+            } else {
+                form.submit();
+            }
+            //return false;
+        }
+    });
     // Enable Autofill Fields
     $(".autofill").autocomplete({
         source : function(request, response){
