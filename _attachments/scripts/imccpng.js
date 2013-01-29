ddd9812a9efc48be3f0e7f33bc847ebb0eb47ffb
David Robbins <robbins.david@gmail.com>
2013-01-29 15:39:56 -0600

    Got admin page working :)

--- _attachments/scripts/imccpng.js
+++ _attachments/scripts/imccpng.js
@@ -9,7 +9,7 @@
     $routeProvider.when("/", {templateUrl : "templates/main/home.html"}).
       when("/patients", {templateUrl : "templates/main/patients.html"}).
       when("/dashboard", {templateUrl : "templates/main/dashboard.html"}).
-      when("/admin", {templateUrl : "templates/main/admin.html"}).
+      when("/admin", {controller : "AdminController", templateUrl : "templates/main/admin.html"}).
       otherwise({redirectTo:"/"});
   });
 
@@ -27,6 +27,7 @@
           });
       },
       logout : function logout () {
+        currentSession = null;
         return $http({method : "DELETE", url : sessionURL});
       },
       loggedIn : function () {
@@ -49,8 +50,9 @@
   });
 
   imccp.factory("User", function ($resource) {
-    return $resource('../../../_users/:id', {id:"@_id"}, {
-      "put" : { method : "PUT" }
+    return $resource('../../../_users/:id', {id:"@id"}, {
+      "put" : { method : "PUT"},
+      "list" : { method : "GET", params : {id:"_all_docs"}}
     });
   });
   
@@ -95,6 +97,7 @@
 
         Session.getSession().then(function (session) {
           $scope.session = session;
+          $scope.$emit("updateNav");
         });
 
         $scope.$watch( function () { return $scope.session; }, function (session) {
@@ -115,6 +118,7 @@
   imccp.controller("NavController", function ($scope, $rootScope, Session) {
     $scope.navbar = "";
     $scope.updateNav = function updateNav() {
+      $scope.navbar = "";
       var user, roles, session;
       session = Session.loggedIn();
       if (session) {
@@ -126,17 +130,62 @@
         }
         if (roles.indexOf("_admin") !== -1) {
           $scope.navbar = "templates/navbars/admin.html";
-        } else if (user.clinic) {
+        } else if (roles.indexOf("nurse") !== -1) {
           $scope.navbar = "templates/navbars/nurse.html";
         } else if (roles.indexOf("dashboard") !== -1) {
           $scope.navbar = "templates/navbars/dashboard.html";
-        } else {
-          $scope.navbar = "";
         }
       }
     };
     $scope.$on("updateNav", $scope.updateNav);
     $rootScope.$on("updateNav", $scope.updateNav);
+  });
+
+  imccp.controller("AdminController", function ($scope) {
+  });
+
+  imccp.controller("UserController", function ($scope, User) {
+    var response = User.list(function () {
+      $scope.users = response.rows.map(function (row) {
+        if (row.id !== "_design/_auth") {
+          row.name = row.id.slice(17);
+          return row;
+        }
+      }).filter(function (row) {
+        if (row) return row;
+      });
+    });
+
+    $scope.clinics = ["", "Breast", "Gi", "Gyn Onc", "Head And Neck", "Lymp And Leuk", "Neuro Onc", "Thoracic", "Urology"];
+    $scope.updatePassword = function updatePassword () {
+      var user = new User($scope.user);
+      user.password = $scope.password;
+      user.$put({id : user._id}, function () {
+        $scope.updateUserModel();
+      });
+    };
+    $scope.updateRoles = function updateRoles () {
+      var updatedRoles = [], user;
+      if ($scope.dashboard) updatedRoles.push("dashboard");
+      updatedRoles.push($scope.clinic);
+      $scope.user.roles = updatedRoles.concat($scope.user.roles.filter(function (role) {
+        if ($scope.clinics.indexOf(role) === -1 && role !== "dashboard") return role;
+      }));
+      user = new User($scope.user);
+      user.$put({id : user._id}, function () {
+        $scope.updateUserModel();
+      });
+    };
+    $scope.updateUserModel = function updateUserModel () {
+      $scope.user = User.get({ "id" : $scope.selected.id}, function () {
+        $scope.dashboard = ($scope.user.roles.indexOf("dashboard") !== -1);
+        $scope.clinic = (function () {
+          return $scope.clinics.filter(function (clinic) {
+            return $scope.user.roles.indexOf(clinic) !== -1 ? clinic : null;
+          })[0];
+        })();
+      });
+    };
   });
 
 })();
