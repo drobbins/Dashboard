44e141429c6f01e38908a2b9c31c2f4e561a7ed6
David Robbins <robbins.david@gmail.com>
2013-02-26 16:00:40 -0600

    Transform numeric MRN's into Strings.

--- views/medrecs/map.js
+++ views/medrecs/map.js
@@ -1,6 +1,9 @@
 function(doc){
   if (doc.type==="data_management_form" && !doc.deleted){
-    var value = { name : doc.ptlstnm+", "+doc.ptfstnm, medrec: doc.medrec, clinic : doc.clinic};
-    emit(doc.medrec, value);
+    var mrn;
+    if (typeof doc.medrec === 'number') mrn = String(doc.medrec);
+    else mrn = doc.medrec;
+    var value = { name : doc.ptlstnm+", "+doc.ptfstnm, medrec: mrn, clinic : doc.clinic};
+    emit(mrn, value);
   }
 }
