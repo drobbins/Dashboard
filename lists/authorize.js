b06aaa3e3d9b58539daa9ea7fcbb7fd2b2cfc35d
David Robbins <robbins.david@gmail.com>
2013-03-01 08:02:30 -0600

    Fixed error when authorize.js list was used on views without a clinic field in the value.

--- lists/authorize.js
+++ lists/authorize.js
@@ -3,7 +3,11 @@ function(head, req){
       row,
       user = req.userCtx,
       has_permission = function(row){
-        if((user.roles.indexOf("_admin") >= 0 || user.roles.indexOf("dashboard") >= 0) || (user.roles.indexOf(row.value.clinic) >= 0)){
+        var clinic = row.value && row.value.clinic;
+        if (user.roles.indexOf("_admin") >= 0 || user.roles.indexOf("dashboard") >= 0) {
+          return true;
+        }
+        if (clinic && (user.roles.indexOf(clinic) >= 0)){
           return true;
         }
         return false;
