88e609b6a492aab46a9e931196987690a26db9ca
David Robbins <robbins.david@gmail.com>
2014-03-27 11:15:06 -0500

    Initial attempt at making tx plans dependent on tx location.

--- _attachments/scripts/imccpng.js
+++ _attachments/scripts/imccpng.js
@@ -778,4 +778,19 @@
         };
     });
 
+    imccp.directive("requiredIf", function () {
+        return {
+            restrict: "A",
+            link: function (scope, element, attr) {
+                scope.$watch(attr.requiredIf, function (required) {
+                    if (required) {
+                        element.attr("required", "true");
+                    } else {
+                        element.removeAttr("required");
+                    }
+                });
+            }
+        };
+    });
+
 })();
